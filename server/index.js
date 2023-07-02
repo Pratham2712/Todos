949e6b6c47403ad079f258d9f53d7f5e648ed7d3
pratham <prathamvaishya123@gmail.com>
2023-07-02 19:32:23 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import express from "express";
-import Connection from './mongo.js';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import Connection from "./mongo.js";
+import cors from "cors";
+import bodyParser from "body-parser";
 import route from "./routes.js";
+Connection();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/',route);
+app.use("/", route);
 const PORT = 5000;
-Connection();
-app.listen(PORT,()=>{
-    console.log(`Listening on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
