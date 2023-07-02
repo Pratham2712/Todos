3ff5eee9d324dd97f6a94a4275d212f8c548aaa1
pratham <prathamvaishya123@gmail.com>
2023-07-02 18:58:51 +0530

    changes

--- server/mongo.js
+++ server/mongo.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
-const Connection = () =>{
-    const mongoUrl = 'mongodb+srv://prathamvaishya:pratham2712@todo.fggkco1.mongodb.net/?retryWrites=true&w=majority'
-    mongoose.connect(mongoUrl,{ useNewUrlParser: true})
-    .then(()=>{console.log('sucessful')})
-    .catch((err)=>{console.log(err.message)})
-}
+const Connection = async () => {
+  const mongoUrl =
+    "mongodb+srv://prathamvaishya:pratham2712@todo.fggkco1.mongodb.net/?retryWrites=true&w=majority";
+  await mongoose
+    .connect(mongoUrl, { useNewUrlParser: true })
+    .then(() => {
+      console.log("sucessful");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 
 export default Connection;
